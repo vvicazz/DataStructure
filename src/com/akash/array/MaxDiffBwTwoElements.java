f5ad541e63e539f6388d0382cbe550028b8e3add
akash-sharma <akash777.sharma@gmail.com>
2015-05-06 00:43:56 +0530

    array questions

---
+++ src/com/akash/array/MaxDiffBwTwoElements.java
@@ -0,0 +1,57 @@
+package com.akash.array;
+
+public class MaxDiffBwTwoElements {
+
+	private int arr[] = { 2, 3, 10, 6, 100, 8, 1 };
+
+	public static void main(String args[]) {
+
+		MaxDiffBwTwoElements ob = new MaxDiffBwTwoElements();
+		System.out.println(ob.findMaxDiff());
+		System.out.println(ob.maxSubArraySum(ob.arr));
+	}
+
+	/*
+	 * time -- O(n) space -- O(1)
+	 */
+	private int findMaxDiff() {
+		int arr[] = this.arr;
+		int minimumNumFound = arr[0];
+		int maximumSumFound = arr[1] - arr[0];
+		for (int counter = 1; counter < arr.length; counter++) {
+			if (arr[counter] - minimumNumFound > maximumSumFound) {
+				maximumSumFound = arr[counter] - minimumNumFound;
+			}
+			if (arr[counter] < minimumNumFound) {
+				minimumNumFound = arr[counter];
+			}
+		}
+		return maximumSumFound;
+	}
+	
+	private int maxSubArraySum(int arr[])
+	{
+	   int max_so_far = 0, max_ending_here = 0;
+	   int i;
+	   for(i = 0; i < arr.length; i++)
+	   {
+	     max_ending_here = max_ending_here + arr[i];
+	     if(max_ending_here < 0)
+	        max_ending_here = 0;
+	     if(max_so_far < max_ending_here)
+	        max_so_far = max_ending_here;
+	    }
+	    return max_so_far;
+	}
+}
+
+/*
+ * Given an array arr[] of integers, find out the difference between any two
+ * elements such that larger element appears after the smaller number in arr[].
+ * 
+ * input : 2, 3, 10, 6, 4, 8, 1 <=> output : 8 (10-2)
+ * 
+ * input : 7, 9, 5, 6, 3, 2 <=> output : 2 (9-7)
+ * 
+ * input : 2, 3, 10, 6, 100, 8, 1 <=> output : 98
+ */
--- src/com/akash/sorting/algo/MergeSort.java
+++ src/com/akash/sorting/algo/MergeSort.java
@@ -26,6 +26,96 @@ public class MergeSort {
 
 	private void merge(int low, int mid, int high) {
 
-		
+		int arr[] = this.arr;
+		int length = high - low + 1;
+		int[] tempArr = new int[length];
+		int i, j, counter = 0;
+
+		// compare each element in two list
+		for (i = low, j = mid + 1; (i <= mid) && (j <= high);) {
+			if (arr[i] < arr[j]) {
+				tempArr[counter] = arr[i];
+				i++;
+			} else {
+				tempArr[counter] = arr[j];
+				j++;
+			}
+			counter++;
+		}
+
+		// if any list ends for comparison,fill rest of the elements for other
+		// list
+		if (i > mid && j <= high) {
+			for (; j <= high; j++, counter++) {
+				tempArr[counter] = arr[j];
+			}
+		} else if (j > high && i <= mid) {
+			for (; i <= mid; i++, counter++) {
+				tempArr[counter] = arr[i];
+			}
+		}
+
+		// put sorted list in original list
+		for (int temp = 0; temp < length; temp++) {
+			arr[low + temp] = tempArr[temp];
+		}
 	}
 }
+
+
+/*
+ 
+complexity : n*log(n)
+
+let number of elements be 8.
+m_s   represents  merge_sort()  method.
+The indentation shows the stack of recursion.
+
+m_s(0,7)
+    m_s(0,3)
+         m_s(0,1)
+                low=0,high=1,mid=0      merge(0,0,1)
+         m_s(2,3)
+                low=2,high=3,mid=2      merge(2,2,3)
+
+         low=0,high=3,mid=1             merge(0,1,3)
+
+    m_s(4,7)
+         m_s(4,5)
+               low=4,high=5,mid=4      merge(4,4,5)
+         m_s(6,7)
+               low=6,high=7,mid=6      merge(6,6,7)
+
+         low=4,high=7,mid=5            merge(4,5,7)
+
+    low=0,high=7,mid=3                 merge(0,3,7)
+
+
+eg:
+array={8,7,6,5,4,3,2,1}
+The part of  underlined  list shows that  part of list is sorted  using  merge()  method.
+
+8 7 6 5 4 3 2 1
+ 
+7 8 6 5 4 3 2 1
+---
+
+7 8 5 6 4 3 2 1
+    ---
+
+5 6 7 8 4 3 2 1
+-------
+
+5 6 7 8 3 4 2 1
+        ---
+
+5 6 7 8 3 4 1 2
+            ---
+
+5 6 7 8 1 2 3 4
+        -------
+
+1 2 3 4 5 6 7 8
+---------------
+
+*/
