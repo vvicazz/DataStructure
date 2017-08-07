9eb101305f7a7c49ff5df0ad5003fe553bdf4b95
akash-sharma <akash777.sharma@gmail.com>
2017-08-07 22:06:39 +0530

    DS notes

--- src/com/akash/docs/DS/DP/MatrixChainMultiplication.java
+++ src/com/akash/docs/DS/DP/MatrixChainMultiplication.java
@@ -19,8 +19,10 @@ public class MatrixChainMultiplication {
 
 	public static void main(String args[]) {
 		int arr[] = { 40, 20, 30, 10, 30 };
-		int minCost = matrixChainRecursive(arr, 0, arr.length - 1);
-		System.out.println(minCost);
+		int minCost1 = matrixChainRecursive(arr, 0, arr.length - 1);
+		int minCost2 = matrixChainDp(arr);
+		System.out.println(minCost1);
+		System.out.println(minCost2);
 	}
 
 	private static int matrixChainRecursive(int arr[], int start, int end) {
@@ -36,5 +38,31 @@ public class MatrixChainMultiplication {
 			}
 		}
 		return minCost;
+	}
+	
+	private static int matrixChainDp(int arr[]) {
+		int memo[][] = new int[arr.length][arr.length];
+		int arrayBreakSol[][] = new int[arr.length][arr.length];
+		for (int i = 0; i < memo.length; i++) {
+			memo[i][i] = 0;
+		}
+		for (int i = 0; i < memo.length - 1; i++) {
+			memo[i][i + 1] = 0;
+		}
+		int tempCost;
+		for (int i = 2; i < memo.length; i++) {
+			for (int j = 0; j + i < memo.length; j++) {
+				int minCost = Integer.MAX_VALUE;
+				for (int k = j + 1; k < j + i; k++) {
+					tempCost = memo[j][k] + memo[k][j + i] + arr[j] * arr[k] * arr[j + i];
+					if (minCost > tempCost) {
+						minCost = tempCost;
+						arrayBreakSol[j][j + i] = k;
+					}
+				}
+				memo[j][j + i] = minCost;
+			}
+		}
+		return memo[0][memo.length - 1];
 	}
 }
