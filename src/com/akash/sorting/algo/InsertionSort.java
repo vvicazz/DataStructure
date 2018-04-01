3e0c82369d79ab3a43614f167853e82b6ea58d2a
akash-sharma <akash777.sharma@gmail.com>
2018-04-01 11:52:07 +0530

    recursive insertion sort

--- src/com/akash/sorting/algo/InsertionSort.java
+++ src/com/akash/sorting/algo/InsertionSort.java
@@ -9,6 +9,7 @@ public class InsertionSort {
 		int arr[] = { 6, 5, 3, 1, 8, 7, 2, 4 };
 		System.out.println(Arrays.toString(arr));
 		insertionSort(arr);
+		// recursiveInsertionSort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 
@@ -26,6 +27,34 @@ public class InsertionSort {
 				}
 			}
 			arr[counterPos] = counterVar;
+		}
+	}
+
+	private static void recursiveInsertionSort(int arr[]) {
+		recursiveInsertion(arr, 0, arr.length - 2);
+		fixInsertionLogic(arr, arr.length - 1);
+	}
+
+	private static void recursiveInsertion(int arr[], int start, int end) {
+		if (start < end) {
+			recursiveInsertion(arr, start, end - 1);
+			fixInsertionLogic(arr, end);
+		}
+	}
+
+	private static void fixInsertionLogic(int arr[], int pos) {
+		int temp = arr[pos];
+		int i = pos - 1;
+		while (i >= 0) {
+			if (temp < arr[i]) {
+				arr[i + 1] = arr[i];
+				i--;
+			} else {
+				break;
+			}
+		}
+		if (i + 1 < pos) {
+			arr[i + 1] = temp;
 		}
 	}
 }
